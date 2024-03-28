de2f42d50b1fc9726e28fba1dd79b49685f6f61a
Константин Скойбеда <konstantsin.skoibeda@novusbel.by>
2024-03-28 09:10:50 +0300

    ICypher impl

--- Interfaces/ICypher.cs
+++ Interfaces/ICypher.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using backup_manager.Cypher;
+using System.Security;
 
 namespace backup_manager.Interfaces
 {
     internal interface ICypher
     {
+        RequisiteInformation Decrypt(SecureString userEncrypted, string uSalt, SecureString passEncrypted, string pSalt);
+        SecureString DecryptString(SecureString encryptedData, string salt);
+        RequisiteInformation Encrypt(SecureString user, SecureString pass);
+        string ToInsecureString(SecureString input);
+        SecureString ToSecureString(string input);
     }
 }
