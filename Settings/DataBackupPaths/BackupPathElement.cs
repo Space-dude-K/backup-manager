0d83ad9b4f751fb94035817112c147db1c61b29c
Константин Скойбеда <konstantsin.skoibeda@novusbel.by>
2024-04-18 15:46:35 +0300

    Backup path collection settings

---
+++ Settings/DataBackupPaths/BackupPathElement.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace backup_manager.Settings.BackupPaths
+{
+    class BackupPathElement : ConfigurationElement
+    {
+        [ConfigurationProperty("path", DefaultValue = "", IsRequired = true)]
+        public string Path
+        {
+            get { return (string)this["path"]; }
+            set { this["path"] = value; }
+        }
+        public BackupPathElement()
+        {
+        }
+    }
+}
---
+++ Settings/DataBackupPaths/BackupPathElementCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace backup_manager.Settings.BackupPaths
+{
+    [ConfigurationCollection(typeof(BackupPathElement), AddItemName = "path", CollectionType = ConfigurationElementCollectionType.BasicMap)]
+    class BackupPathElementCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new BackupPathElement();
+        }
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            if (element == null)
+                throw new ArgumentNullException("Path element is null.");
+
+            return ((BackupPathElement)element).Path;
+        }
+    }
+}
