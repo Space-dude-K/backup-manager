9320865cae8ed6c1d0fe2ec3c36420d093c7a3ae
Константин Скойбеда <space.dude.work@gmail.com>
2024-06-18 16:39:55 +0300

    Shell worker refactoring

--- Extensions.cs
+++ Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace backup_manager
 {
@@ -50,7 +51,49 @@ namespace backup_manager
         }
         internal static string GetCleanFileName(this string rawFileName)
         {
-            return string.Join("", rawFileName.Split(Path.GetInvalidFileNameChars())).Replace(" ", "");
+            var fileName = string.Join("", rawFileName.Split(Path.GetInvalidFileNameChars())).Replace(" ", "");
+
+            if(fileName.Length > 63)
+                throw new Exception("File name to long exception. (fn must be less than 63 characters");
+
+            return fileName;
+        }
+        public static string StringBeforeLastRegEx(this string str, Regex regex)
+        {
+            var matches = regex.Matches(str);
+
+            return matches.Count > 0
+                ? str.Substring(0, matches.Last().Index)
+                : str;
+
+        }
+
+        public static bool EndsWithRegEx(this string str, Regex regex)
+        {
+            var matches = regex.Matches(str);
+
+            return
+                matches.Count > 0 &&
+                str.Length == (matches.Last().Index + matches.Last().Length);
+        }
+
+        public static string StringAfter(this string str, string substring)
+        {
+            var index = str.IndexOf(substring, StringComparison.Ordinal);
+
+            return index >= 0
+                ? str.Substring(index + substring.Length)
+                : "";
+        }
+
+        public static string[] GetLines(this string str) =>
+            Regex.Split(str, "\r\n|\r|\n");
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> func)
+        {
+            foreach (var item in sequence)
+            {
+                func(item);
+            }
         }
     }
 }
