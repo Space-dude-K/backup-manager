f4cabcc34cf4e60ff6ca8fff339edb08a7a4ca79
Константин Скойбеда <space.dude.work@gmail.com>
2024-06-27 16:58:47 +0300

    Ftp server imlp

--- Interfaces/IFtpServer.cs
+++ Interfaces/IFtpServer.cs
@@ -1,6 +1,9 @@
-﻿namespace backup_manager.Interfaces
+﻿
+namespace backup_manager.Interfaces
 {
     internal interface IFtpServer
     {
+        void RunFtpServer(string tempDir);
+        Task<bool> RunFtpServerAsync(string tempDir, int serverDlTimeRangeInMs = 30000);
     }
 }
